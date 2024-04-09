2d09b2b79e613ab1a7bb73f576b258b8824808c9
lchenevi <leo.cheneviere@gmail.com>
2024-04-09 14:18:04 +0200

    slight change in task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,47 +4,26 @@ Retourn html page
 
 
 """
-from flask import Flask
+from flask import Flask, escape
 
-# Create a Flask application instance
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
-    """
-    This function handles requests to the root URL ("/").
-    """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
-
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    This function handles requests to the "/hbnb" URL.
-    """
-    return "HBNB"
+    return 'HBNB'
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C {}'.format(escape(text.replace('_', ' ')))
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """
-    This function handles requests to the "/c/<text>" URL
-    """
-    text = text.replace("_", " ")
-    return f"C {text}"
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
 
-
-@app.route("/python", defaults={"text": "is cool"})
-@app.route("/python/<text>", strict_slashes=False)
-def python(text):
-    """
-    This function handles requests to the "/python/<text>" URL
-    """
-    text = text.replace("_", " ")
-    return f"Python {text}"
-
-
-if __name__ == "__main__":
-    # Run the Flask application
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
