76f1e5f1f592dd4c80b08abba5442561e3a7e0d7
lchenevi <leo.cheneviere@gmail.com>
2024-04-11 10:16:46 +0200

    pushing task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""
-Retourn html page
-
-
-"""
+"""Module for the different pages with Flask"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
-# Create a Flask application instance
+
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Returns the list of all cities in the database."""
-    states = sorted(storage.all("State").values(), key=lambda s: s.name)
-    for state in states:
-        state.cities = sorted(state.cities, key=lambda c: c.name)
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def close(self):
-    """Closes the database session."""
+def close(exception):
+    """Close storage"""
     storage.close()
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_states():
+    """List all cities by states"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State))
+
+
 if __name__ == "__main__":
-    # Run the Flask application
     app.run(host="0.0.0.0", port=5000)
