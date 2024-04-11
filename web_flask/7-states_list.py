1477da92c22498abe09cfc8d1b9e6dad5fcfeeb9
lchenevi <leo.cheneviere@gmail.com>
2024-04-11 10:19:54 +0200

    pushing task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-"""
-Retourn html page
-
-
-"""
+"""Module for the different pages with Flask"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-# Create a Flask application instance
+
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Returns the list of all States in the database."""
-    states = {}
-    for state in sorted(storage.all("State").values(), key=lambda s: s.name):
-        states[state.id] = state.name
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def close(self):
-    """Closes the database session."""
+def close(exception):
+    """Close storage"""
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """List all states"""
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=sorted_states)
+
+
 if __name__ == "__main__":
-    # Run the Flask application
     app.run(host="0.0.0.0", port=5000)
