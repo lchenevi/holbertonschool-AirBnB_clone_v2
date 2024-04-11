becb92f5c358ed97db32cac6966ef6cd9406a165
lchenevi <leo.cheneviere@gmail.com>
2024-04-11 10:23:57 +0200

    pushing task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-"""
-Retourn html page
-
-
-"""
+"""Module for the different pages with Flask"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
-# Create a Flask application instance
+
 app = Flask(__name__)
 
 
-@app.route("/states", defaults={"id": None})
-@app.route("/states/<id>", strict_slashes=False)
-def states(id):
-    """Returns the list of all cities and state in the database."""
-    states = sorted(storage.all("State").values(), key=lambda s: s.name)
-    for state in states:
-        state.cities = sorted(state.cities, key=lambda c: c.name)
-    if  id is not None:
-        id = 'State.' + id
-        states = storage.all("State")
-
-    return render_template("9-states.html", states=states, id=id)
-
-
 @app.teardown_appcontext
-def close(self):
-    """Closes the database session."""
+def close(exception):
+    """Close storage"""
     storage.close()
 
 
+@app.route("/states", strict_slashes=False)
+def states():
+    """List all states"""
+    return render_template("9-states.html", states=storage.all(State).values())
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def cities_in_state(id):
+    """List all cities in the state id that was given"""
+    states = storage.all(State).values()
+    state = None
+    for obj in states:
+        if id == obj.id:
+            state = obj
+    return render_template('9-states.html', state=state)
+
+
 if __name__ == "__main__":
-    # Run the Flask application
     app.run(host="0.0.0.0", port=5000)
