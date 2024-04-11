52b0548b6808aea0682ea13a1b29b58a2cb8b543
lchenevi <leo.cheneviere@gmail.com>
2024-04-11 10:33:51 +0200

    pushing task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = sorted(storage.all("State").values(), key=lambda x: x.name)
+    cities = sorted(storage.all("City").values(), key=lambda x: x.name)
+    amenities = sorted(storage.all("Amenity").values(), key=lambda x: x.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
