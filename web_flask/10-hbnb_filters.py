52a419f27792cd8393229c680b82af6de786eb5c
lchenevi <leo.cheneviere@gmail.com>
2024-04-11 10:36:07 +0200

    pushing task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
+"""Module for the different pages with Flask"""
 from flask import Flask, render_template
 from models import storage
 
+# Create a Flask application instance
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
-    states = sorted(storage.all("State").values(), key=lambda x: x.name)
-    cities = sorted(storage.all("City").values(), key=lambda x: x.name)
-    amenities = sorted(storage.all("Amenity").values(), key=lambda x: x.name)
-    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+    """Returns the list of all cities in the database."""
+    states = sorted(storage.all("State").values(), key=lambda s: s.name)
+    amenity = sorted(storage.all("Amenity").values(), key=lambda s: s.name)
+    for state in states:
+        state.cities = sorted(state.cities, key=lambda c: c.name)
+    return render_template("10-hbnb_filters.html", states=states, amenity=amenity)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def close(self):
+    """Closes the database session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Run the Flask application
+    app.run(host="0.0.0.0", port=5000)
