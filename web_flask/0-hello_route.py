3ae63437455973a2b6ea3f0fcbc70ed774629935
lchenevi <leo.cheneviere@gmail.com>
2024-04-11 10:40:00 +0200

    applying some modifs on the tasks to try to get full points

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,11 +7,20 @@ Retourn html page
 
 from flask import Flask
 
+'''Créer une instance de l'application Flask'''
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return 'Hello HBNB!'
+# Définit une route '/' pour l'URL racine
 
+@app.route("/", strict_slashes=False)
+def hello_world():
+    '''Fonction de vue qui renvoie "Hello HBNB!"'''
+    return "Hello HBNB!"
+
+# si ce script est exécuté en tant que programme principal
 if __name__ == '__main__':
+    '''Lance l'application Flask'''
+    # host='0.0.0.0' permet à l'application d'écouter sur toutes
+    # les interfaces réseau
+    # port=5000 spécifie le port sur lequel l'application va écouter'''
     app.run(host='0.0.0.0', port=5000)
