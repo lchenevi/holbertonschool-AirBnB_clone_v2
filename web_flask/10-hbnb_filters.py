ed8f0cef699c0805901a9f9f6dc406ed5cc01106
lchenevi <leo.cheneviere@gmail.com>
2024-04-11 10:51:20 +0200

    applying some modifs on the tasks to try to get full points

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
-"""Module for the different pages with Flask"""
+"""
+flask model
+"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+from models.amenity import Amenity
 
-# Create a Flask application instance
 app = Flask(__name__)
-
-
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """Returns the list of all cities in the database."""
-    states = sorted(storage.all("State").values(), key=lambda s: s.name)
-    amenity = sorted(storage.all("Amenity").values(), key=lambda s: s.name)
-    for state in states:
-        state.cities = sorted(state.cities, key=lambda c: c.name)
-    return render_template("10-hbnb_filters.html", states=states, amenity=amenity)
+storage.all()
 
 
 @app.teardown_appcontext
-def close(self):
-    """Closes the database session."""
+def teardown_data(self):
+    """
+        refrech data
+    """
     storage.close()
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def filter(id=None):
+    """
+    Display a page for hbnb_filters
+    State, City and Amenity objects must be loaded from DBStorage
+    """
+    data = storage.all(State)
+    states = []
+    for k in data:
+        states.append(data[k])
+
+    data = storage.all(Amenity)
+    amenities = []
+    for k in data:
+        amenities.append(data[k])
+
+    return render_template('10-hbnb_filters.html', states=states,
+        amenities=amenities)
 
 if __name__ == "__main__":
-    # Run the Flask application
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
